894e60fa78ad39f7ad58d13125bdb29944acb889
Adriel Ticzon <adrielticzon@gmail.com>
2022-09-02 13:25:20 +0800

    ys

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ try {
   core.setOutput("result", "ok")
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
   console.log('curent pr', payload.pull_request.self);
+  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
